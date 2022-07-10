d40f78e50735292dfb0e41620ad62997408570bd
jakubvanko <kubko.vanko@gmail.com>
2022-07-10 19:40:46 +0200

    feat(backend): add cors and helmet security packages

--- backend/scripts/app.js
+++ backend/scripts/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import createError from "http-errors";
+import helmet from "helmet";
+import cors from "cors";
 // Handle async errors without try and catch blocks
 // consider adding require("express-async-errors");
 
 const app = express();
+// Can set cors origin for better security
+app.use(cors());
+app.use(helmet());
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 // If form objects needed, use: app.use(express.urlencoded({ extended: false }));
