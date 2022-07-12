909d83b1d7d132cfbf723d0fbcc8e230e8486326
jakubvanko <kubko.vanko@gmail.com>
2022-07-13 00:35:56 +0200

    feat(frontend): add react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { Header } from "./components/Header/Header";
 import { LoginPage } from "./components/LoginPage/LoginPage";
 import { ThemeProvider } from "@emotion/react";
 import { UserPage } from "./components/UserPage/UserPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BasicMessagePage } from "./components/BasicMessagePage/BasicMessagePage";
 
 const theme = createTheme({
   palette: {
@@ -16,12 +18,44 @@ const theme = createTheme({
 const App = () => {
   return (
     <StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline>
-          <Header />
-          <UserPage />
-        </CssBaseline>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <Header />
+            <Routes>
+              <Route path="login/:token" element={<LoginPage />} />
+              <Route path="user" element={<UserPage />} />
+              <Route
+                path="logout"
+                element={
+                  <BasicMessagePage
+                    headingText="Boli ste odhlásení"
+                    descriptionText="Boli ste úspešne odhlásení. Teraz môžete webstránku bezpečne opustiť."
+                  />
+                }
+              />
+              <Route
+                path="deleted"
+                element={
+                  <BasicMessagePage
+                    headingText="Váš prihlasovací odkaz bol zneplatnený"
+                    descriptionText="Váš prihlasovací odkaz bol zneplatnený. Ďakujeme za účasť v našej štúdii."
+                  />
+                }
+              />
+              <Route
+                path="*"
+                element={
+                  <BasicMessagePage
+                    headingText="Chyba 404"
+                    descriptionText="Zadaná stránka neexistuje alebo bola odstránená."
+                  />
+                }
+              />
+            </Routes>
+          </CssBaseline>
+        </ThemeProvider>
+      </BrowserRouter>
     </StrictMode>
   );
 };
