c43c07e77d6d5c186d340db69321be15da1dca23
jakubvanko <kubko.vanko@gmail.com>
2022-07-17 18:42:05 +0200

    refactor(backend): rename root folder to src

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import http from "http";
 import mongoose from "mongoose";
-import app from "./scripts/app.js";
+import app from "./src/app.js";
 
 mongoose.connect("mongodb://localhost:27017", {}, (err) => {
   if (err) throw err;
